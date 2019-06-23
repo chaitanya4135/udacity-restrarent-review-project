3e132dbef6669689dfad8588ed09aebef90062bb
chaitanya4135 <48980947+chaitanya4135@users.noreply.github.com>
2019-06-23 21:08:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -160,7 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = "./"+DBHelper.imageUrlForRestaurant(restaurant);
   image.alt=restaurant.name;
   li.append(image);
 
